9e648b0dfff604456fdbfd5cd2405ff52ae06eed
Zach Frohardt <zfrohardt@gmail.com>
2021-05-20 18:25:11 -0700

    commit tree 85d4681ab03db5bb5333f3a342edcfced7539e3f

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with? "."
   end
 
   def question?
-
+    self.end_with? "?"
   end
 
   def exclamation?
-
+    self.end_with? "!"
   end
 
   def count_sentences
-
+    self.split(/[.!?]/).reject { |s| s.length == 0 }.count
   end
 end
